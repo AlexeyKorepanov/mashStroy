7c4fc81065eefc8d10b37662ad8236b207cfbe2c
AlexeyKorepanov <74230259+AlexeyKorepanov@users.noreply.github.com>
2020-11-14 15:02:53 +0400

    Update Norm.java

--- src/raschet/Norm.java
+++ src/raschet/Norm.java
@@ -19,7 +19,7 @@ public class Norm extends javax.swing.JFrame {
     }
   
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    
     private void initComponents() {
 
         try {
